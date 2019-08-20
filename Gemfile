6334aa358e848026c6bd18d3db278e50ef9ca12d
Simon Isler <simon-isler@gmx.ch>
2019-08-20 12:33:11 +0200

    Add register function

--- Gemfile
+++ Gemfile
@@ -2,4 +2,7 @@ source 'https://rubygems.org'
 ruby '2.6.2'
 
 gem 'rubocop'
-gem 'mechanize', '~> 2.7', '>= 2.7.5'
+gem 'capybara'
+gem 'selenium-webdriver'
+
+
--- src/bot.rb
+++ src/bot.rb
@@ -1,6 +1,41 @@
-require 'rubygems'
-require 'mechanize'
+require 'capybara'
+require 'capybara/dsl'
+require 'selenium/webdriver'
 
-agent = Mechanize.new
-page = agent.get('http://google.com/')
+Capybara.run_server = false
+# Capybara.current_driver = :selenium_chrome_headless
+Capybara.current_driver = :selenium_chrome
 
+class Bot
+  include Capybara::DSL
+
+  def initialize
+    puts '--------------------'
+    puts 'Energyair-Bot 2019'
+    puts '--------------------'
+
+    visit('https://game.energy.ch')
+  end
+
+  def register
+    print 'Please enter your phone number: '
+    tel_number = gets.chomp
+    fill_in('inlineFormInput', with: tel_number)
+    click_button('Verifizieren')
+
+    print 'Please enter the activation code: '
+    activation_code = gets.chomp
+    numbers = activation_code.split('')
+    numbers.each_with_index do |number, index|
+      fill_in((index+1).to_s, with: number)
+    end
+    click_button('Verifizieren')
+  end
+
+  def run
+    register
+  end
+end
+
+bot = Bot.new
+bot.run
